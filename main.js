e1195248a6726f8eb390f135268e047a9c852b79
Hady-ali <haaaady7@gmail.com>
2024-08-22 15:55:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// flutter
+
+// List <dynamic> fruits = ["apple","banana","orang"];
+// print(fruits[1]);
+
+// Set <String> country = { "USA","canada","mixeco" };
+// print(country);
+
+// Map person = {
+//     "name":"hady",
+//     "age":20,
+//     "city":"ElMinya"
+// };
+// print(person["age"]);
+
+// List <int> integer = [1,2,3,4,5,6,7];
+// print(integer[1]==integer[3]);
+// print(integer[2]<=integer[4]);
+// print(integer[3]>=integer[5]);
+// print(integer[5]!=integer[4]);
+
+// Set <int> integers = [1,2,3,4,5,6,7];
+// print(integers[1]==integers[3]);
+// print(integers[2]<=integers[4]);
+// print(integers[3]>=integers[5]);
+// print(integers[5]!=integers[4]);
+
+// Map integer = {
+// "1":1,
+// "2":2,
+// "3":3,
+// "4":4,
+// "5":5,
+// "6":6,
+// };
+// print(integer["1"]==integer["3"]);
+// print(integer["2"]<=integer["4"]);
+// print(integer["3"]>=integer["5"]);
+// print(integer["5"]!=integer["4"]);
+
+
